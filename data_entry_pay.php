2cd0a1e66735465c7029551a68d3bc402697403e
prerna2021 <51856236+prerna2021@users.noreply.github.com>
2019-07-01 00:11:35 +0530

    Update data_entry_pay.php

--- data_entry_pay.php
+++ data_entry_pay.php
@@ -30,30 +30,31 @@
             </table>
     
 
-            <h1 class="text-center">Personal Information</h1>
+           <side lift>&nbsp;<big><big><b>व्यक्तिगत जानकारी </b></big></big><b></b><br>
+		<style= "padding: 7px;"><font size="6"><b>Personal Information</b></font></style><br>
             <div class="data_ent_pay">
                 <table>
 
                     <tr>
-                        <td>Type</td>
+                        <td>प्रकार<br>Type</td>
                         <td>
                             <input type="text" name="type" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>S.No.</td>
+                        <td>क्र. संख्या<br>S.No.</td>
                         <td>
                             <input type="number" name="emp_id" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Name</td>
+                        <td>नाम<br>Name</td>
                         <td>
                             <input type="text" name="name" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Designation</td>
+                        <td>पद<br>Designation</td>
                         <td>
                             <select class="form-control" name="designation">
                                 <option value="scientist">Scientist</option>
@@ -63,7 +64,7 @@
                         </td>
                     </tr>
                     <tr>
-                        <td>Sex</td>
+                        <td>लिंग<br>Sex</td>
                         <td>
                             <select class="form-control" name="sex">
                                 <option value="male">Male</option>
@@ -72,31 +73,31 @@
                         </td>
                     </tr>
                     <tr>
-                        <td>Date Of Birth</td>
+                        <td>जन्म की तारीख<br>Date Of Birth</td>
                         <td>
                             <input type="date" name="birth_date" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Date Of Joining</td>
+                        <td>जॉइनिंग की तारीख<br>Date Of Joining</td>
                         <td>
                             <input type="date" name="joining_date" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Date Of Next Incr.</td>
+                        <td>अगली वेतन वृद्धि की तारीख<br>Date Of Next Incr.</td>
                         <td>
                             <input type="date" name="next_incr_date" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>GPF A/C No.</td>
+                        <td>जीपीएफ खाता नं<br>GPF A/C No.</td>
                         <td>
                             <input type="text" name="gpf_acno" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Payband</td>
+                        <td>वेतन पट्टा<br>Payband</td>
                         <td>
                             <input type="text" name="payband" class="form-control" />
                         </td>
@@ -105,24 +106,24 @@
                 </table>
             </div>
 
-            <h1 class="text-center">CREDITS</h1>
+            <h1 class="text-center">क्रेडिट<br>CREDITS</h1>
             <div class="data_ent_pay">
                 <table>
 
                     <tr>
-                        <td>Basic</td>
+                        <td>मूल<br> Basic</td>
                         <td>
                             <input type="text" name="basic" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>GradePay</td>
+                        <td>ग्रेड पे<br>GradePay</td>
                         <td>
                             <input type="text" name="grade_pay" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Special</td>
+                        <td>विशेष<br>Special</td>
                         <td>
                             <input type="text" name="special" class="form-control" />
                         </td>
@@ -140,13 +141,13 @@
                         </td>
                     </tr>
                     <tr>
-                        <td>HRA</td>
+                        <td>एचआरए<br>HRA</td>
                         <td>
                             <input type="text" name="hra" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Misc.</td>
+                        <td>विविध<br>Misc.</td>
                         <td>
                             <input type="text" name="misc_cre" class="form-control" />
                         </td>
@@ -158,19 +159,19 @@
                         </td>
                     </tr>
                     <tr>
-                        <td>Two Add. Inc</td>
+                        <td>दो अतिरिक्त वृद्धि<br>Two Add. Inc</td>
                         <td>
                             <input type="text" name="two_add_inc" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Variable Inc</td>
+                        <td>परिवर्तनशील वृद्धि<br>Variable Inc</td>
                         <td>
                             <input type="text" name="variable_inc" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Total Credits</td>
+                        <td>कुल क्रेडिट<br>Total Credits</td>
                         <td>
                             <input type="text" name="total_credits" class="form-control" />
                         </td>
@@ -178,24 +179,24 @@
                 </table>
             </div>
 
-            <h1 class="text-center">DEBITS</h1>
+            <h1 class="text-center">डेबिट<br>DEBITS</h1>
             <div class="data_ent_pay">
                 <table>
 
                     <tr>
-                        <td>GPF Contri.</td>
+                        <td>जीपीएफ योगदान<br>GPF Contri.</td>
                         <td>
                             <input type="text" name="gpfcont" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Income Tax</td>
+                        <td>आयकर<br>Income Tax</td>
                         <td>
                             <input type="text" name="it" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Edu cess</td>
+                        <td>शिक्षा उपकर<br>Edu cess</td>
                         <td>
                             <input type="text" name="edu_cess" class="form-control" />
                         </td>
@@ -207,19 +208,19 @@
                         </td>
                     </tr>
                     <tr>
-                        <td>CGHS</td>
+                        <td>सीजीएचएस<br>CGHS</td>
                         <td>
                             <input type="text" name="cghs" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Rent Recov.</td>
+                        <td>किराया वसूल<br>Rent Recov.</td>
                         <td>
                             <input type="text" name="rentrec" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Misc.</td>
+                        <td>विविध<br>Misc.</td>
                         <td>
                             <input type="text" name="misc_deb" class="form-control" />
                         </td>
@@ -231,43 +232,43 @@
                         </td>
                     </tr>
                     <tr>
-                        <td>HBA Loan</td>
+                        <td>एचबीए ऋण<br>HBA Loan</td>
                         <td>
                             <input type="text" name="hba" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>GPF Loan</td>
+                        <td>जीपीएफ ऋण<br>GPF Loan</td>
                         <td>
                             <input type="text" name="gpf_loan" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Vehicle Loan</td>
+                        <td>वाहन ऋण<br>Vehicle Loan</td>
                         <td>
                             <input type="text" name="vehicle_loan" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Festival Loan</td>
+                        <td>त्यौहार ऋण<br>Festival Loan</td>
                         <td>
                             <input type="text" name="festival_loan" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Computer Loan</td>
+                        <td>कंप्यूटर ऋण<br?Computer Loan</td>
                         <td>
                             <input type="text" name="computer_loan" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Other Loan</td>
+                        <td>अन्य ऋण<br>Other Loan</td>
                         <td>
                             <input type="text" name="other_loan" class="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td>Total Debits</td>
+                        <td>कुल डेबिट<br>Total Debits</td>
                         <td>
                             <input type="text" name="total_debits" class="form-control" />
                         </td>
@@ -279,7 +280,7 @@
             <div>
                 <table>
                     <tr>
-                        <td>Net Payable</td>
+                        <td>कुल चुकाया <br>Net Payable</td>
                         <td>
                             <input type="text" name="net_payable" class="form-control" />
                         </td>
